31f39c87e78afcf73ca30e703f095f359db9c02a
Steve Bradshaw <steve@pcfive.co.uk>
2021-08-04 16:41:01 +0100

    config name

--- ubiquitous_contactform/apps.py
+++ ubiquitous_contactform/apps.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class MyWEnquiriesConfig(AppConfig):
+class UbiquitousContactConfig(AppConfig):
     name = "ubiquitous_contact"
     verbose_name = "Ubiquitous Contact Form"
