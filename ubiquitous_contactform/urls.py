af2a03f5c32dee927a82b1f23e02b311b56a314c
Steve Bradshaw <steve@pcfive.co.uk>
2023-01-30 12:22:20 +0000

    Django 4.1

--- ubiquitous_contactform/urls.py
+++ ubiquitous_contactform/urls.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
-from django.conf.urls import url
+from django.urls import re_path
 from ubiquitous_contactform import views as enquiry_views
 
 app_name = "ubiquitous_contactform"
 urlpatterns = [
-    url(r'^contact/?$', enquiry_views.ContactView.as_view(), name='url_contact'),
-    url(r'^thanks/?$', enquiry_views.ThanksView.as_view(), name='url_thanks'),
+    re_path(r'^contact/?$', enquiry_views.ContactView.as_view(), name='url_contact'),
+    re_path(r'^thanks/?$', enquiry_views.ThanksView.as_view(), name='url_thanks'),
 ]
 
